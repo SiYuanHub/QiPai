d87eda3d2dc14b31f25dc1adcc93c040db21caa3
KooMan <xuyameng.shi@gmail.com>
2021-01-14 09:49:08 +0800

    csy

---
+++ cline-app/src/page/HomePage/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home_page">
+    <div class="header">
+
+    </div>
+    <div class="content">
+
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name:"HomePage"
+}
+</script>
+<style lang='less' scoped>
+  .header{
+    height: 100px;
+  }
+  .content{
+    height: calc(100% - 100px);
+  }
+</style>
